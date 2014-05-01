5f36f4c9c246d02f20466973a4f1a50d2703f694
nevermore2014 <zhou.hongyu8976@gmail.com>
2014-05-01 01:14:44 -0400

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+FacebookCard::Application.config.secret_key_base = '2512e7ecd1b42ca04391c1978f5ce5959de7d513ef2861e3e843c27ece68c9ad52fc71e866a31153fd6d5e064b6c0701e179c5a19a2a769afa4c73a1cdf3b5f5'
