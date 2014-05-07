220ca5156dc17eccabd75a36581c0d094bc23299
nevermore2014 <zhou.hongyu8976@gmail.com>
2014-05-06 20:01:33 -0400

    some layout stuff

--- app/assets/javascripts/user.js
+++ app/assets/javascripts/user.js
@@ -26,7 +26,7 @@ FacebookCard.getCardData = function(user_id){
 };
 
 FacebookCard.drawCard = function(callback){
-  var $cardZone = $('.cardZone'),
+  var $cardZone = $('<div class="cardZone">'),
       $insertCard = $('<div class="insertCard">'),
       $info_div = $('<div class="col-md-7 info">'),
       $img_div = $('<div class="col-md-1 img">'),
@@ -38,13 +38,13 @@ FacebookCard.drawCard = function(callback){
       $count = $('<p>likes:' + callback.num_like + ' comments:' + callback.num_comment + '  photos:' + callback.num_photo + '  status:' + callback.num_status + ' links:' + callback.num_link + '  videos:' + callback.num_video + '</p>'),
       $basic_div = $('<div class="basic">');
 
-  $cardZone.text("");
   $info_div.append($name, $location);
   $img_div.append($img);
   $basic_div.append($img_div, $info_div);
   $count_div.append($count);
   $insertCard.append($basic_div, $count_div);
   $cardZone.append($insertCard);
+  $('body').append($cardZone);
 
 
 };
